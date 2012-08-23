f93adca76f3da3879874f416819e9376154a1c7f
Daniel MM <daxx4182@gmail.com>
2012-08-23 20:11:49 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 add_lunch_combo pa_i9100-userdebug
-add_lunch_combo pa_i9300-userdebug
 add_lunch_combo pa_maguro-userdebug
 add_lunch_combo pa_toro-userdebug
 add_lunch_combo pa_toroplus-userdebug
 add_lunch_combo pa_grouper-userdebug
 add_lunch_combo pa_epicmtd-userdebug
+add_lunch_combo pa_crespo-userdebug
