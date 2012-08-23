0a7cc9ac1ff0a9ab5e46618157a2d67136454f12
Daniel MM <daxx4182@gmail.com>
2012-08-23 21:34:07 +0800

    Add support for crespo

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -19,3 +19,6 @@ endif
 ifeq (pa_epicmtd,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_epicmtd.mk
 endif
+ifeq (pa_crespo,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_crespo.mk
+endif
